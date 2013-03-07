789f764edca8eacc94c2d6052d2770440ef0d687
Kate Marvel <marvel1@llnl.gov>
2013-03-07 14:18:16 -0800

    locate_cmip5.py added: rapid search through root directory

---
+++ locate_cmip5.py
@@ -0,0 +1,32 @@
+#!/usr/local/uvcdat/latest/bin/python
+import sys, getopt
+from splice_dictionary import find_files
+
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv,"-s" , ['realm=',\
+                                                    'version=',\
+                                                    'rip=',\
+                                                    'time_frequency=',\
+                                                    'experiment=',\
+                                                    'tableid=',\
+                                                    'variable=',\
+                                                    'model=',\
+                                                    'root='])
+
+    except getopt.GetoptError:
+        print 'Usage: locate_cmip5.py --realm <realm> --version <version>  --rip <rip>'
+        sys.exit(2)
+    d = {}
+    print_search_string = False
+    for opt, arg in opts:
+        if opt == '-s':
+            print_search_string = True
+        else:
+            key = opt.split("--")[-1]
+            d[key] = arg
+   
+    for x in find_files(d):
+        print x
+if __name__ == "__main__":
+   main(sys.argv[1:])
--- splice_dictionary.py
+++ splice_dictionary.py
@@ -4,6 +4,7 @@ import numpy as np
 import fnmatch
 from datetime import datetime
 import cdms2 as cdms
+from string import upper,lower
 
 def locate(pattern, root=os.curdir):
     '''Locate all files matching supplied filename pattern in and below
@@ -35,10 +36,17 @@ def parse_filename(fname):
 
 def newest_version(listoffiles):
     versions = np.array([x.split("ver-")[1].split(".")[0] for x in listoffiles])
-    # check for versions beginning v* 
-    I=  np.where(np.array([v.find("201") <0 for v in versions]))[0]
-    if len(I)>0:
-        versions = versions[I]
+    d = parse_filename(listoffiles[0])
+    model = d["model"]
+    version_numbers = [float(x.split("v")[-1]) for x in versions]
+    if upper(model) == "CSIRO":
+        less_than_1000 = np.where(np.array(version_numbers <1000.))
+        I = np.argmax(version_numbers[less_than_1000])
+    else:
+         I = np.argmax(version_numbers)
+         
+    return listoffiles[I]
+    
     
 
 def check_parentage(fname):
@@ -57,16 +65,20 @@ def check_parentage(fname):
     if meta_rip != parent_rip:
         print "model rip and parent rip do not match"
         return 1
-    d["experiment"] = openf.attributes["parent_experiment"]
+    parent = openf.attributes["parent_experiment"]
+    if parent != "historical":
+        print "parent experiment is "+parent
+        return 2
+    d["experiment"] = parent
     d["version"] = "*"
-    return [x for x in find_files(d)]
+    return newest_version([x for x in find_files(d)])
     
     
     
         
         
 
-def find_files(d):
+def find_files(d,print_search_string = False):
     '''Return all datafiles that match criteria specified in the dictionary d'''
     template = "%(root)%(experiment)/%(realm)/%(time_frequency)/%(variable)/cmip5.%(model).%(experiment).%(rip).mo.%(realm).%(tableid).%(variable).%(version).xml"
     
@@ -80,7 +92,8 @@ def find_files(d):
     #set filename attributes
     
     [setattr(filename,k,d[k]) for k in d.keys()]
-    print filename()
+    if print_search_string:
+        print filename()
    #Get most complete specified directory
     
 
