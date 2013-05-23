2e52caf18f93940b67e7b552ec8fbb64a9fb3dd1
Charles Doutriaux <doutriaux1@llnl.gov>
2013-05-23 16:38:34 -0700

    Almost there, in the end using subprocess on 7za directly, much faster

--- test/test_CMIP5_Reader.py
+++ test/test_CMIP5_Reader.py
@@ -1,7 +1,15 @@
 import CMIP5
-R = CMIP5.Reader(root=".",archiveTemplate="./_archives/%(date)_%(N)_cmip5_xml.7z")
+reload(CMIP5)
+
+import os
+if os.uname()[1]=="crunchy.llnl.gov":
+    R=CMIP5.Reader()
+else:
+    R = CMIP5.Reader(root=".",archiveTemplate="./_archives/%(date)_%(N)_cmip5_xml.7z")
 
 print "-%s-" % R.findArchive()
 R.model = "CNRM-CM5"
 
-print R.open("tas")
+t = R.open("tas")
+p = R.open("pr")
+
